8401c7bd2cd4c32de1cfb18b5b368866f757a8e2
akbarlisamir <samir90319@gmail.com>
2019-11-26 19:54:31 +0400

    Initializing the project...

---
+++ CheApp/CheApp/CheApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheApp.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryID { get; set; }
+        public int MarketID { get; set; }
+    }
+}
---
+++ CheApp/CheApp/CheApp/ProductEntryPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductEntryPage : ContentPage
+    {
+        public ProductEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
